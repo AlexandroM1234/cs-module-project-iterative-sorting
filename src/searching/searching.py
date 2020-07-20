ac69e487b3ba6037c73ee5da643a0e786e4735ba
Alex Martinez <alexmartinez0071234@gmail.com>
2020-07-20 18:20:04 -0400

    binary search with comments

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,8 +11,19 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
-
-
+    first = 0 
+    last = len(arr)-1
+    while  first <= last:
+    # Guess if the target is the half way point
+        middle = (first + last) // 2
+        if arr[middle] == target:
+            return middle
+        else:
+            # if its less than the half make the start the half - 1
+            if target < arr[middle]:
+                last = middle - 1
+             # if its more than the half make the end the half + 1
+            else:
+                first = middle + 1
     return -1  # not found
